e9778f243b5d32c2d7f0a535a1d34a2c379b2a62
佐藤 <alicya378badapple@gmail.com>
2016-12-25 18:07:50 +0900

    環境情報の作成 フェイズ情報の追加

---
+++ maptest/Assets/Scripts/Common.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace Common {
+
+    // 座標データ
+	public struct POSS_DATA {
+		public int index;	// インデックス
+		public uint x;	// X座標
+        public uint y;	// Y座標
+        public uint z;	// Z座標
+        public string type; //マスタイプ
+        public int nomalValue; //値１
+        public int trapValue; //値２
+        public string environment; //環境情報
+	}
+
+// ファイルデータ
+	public struct FILE_DATA {
+		public POSS_DATA[ ] mass; // マス配列
+	}
+
+	public enum PHASE {
+		DICE_PHASE,
+		MOVE_PHASE,
+		DRAW_PHASE,
+		BATTLE_PHASE,
+		RESULT_PHASE,
+		EVENT_PHASE,
+		PHASE_NUM
+	}
+}
---
+++ maptest/Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Common;
+
+public class PhaseManager : MonoBehaviour {
+
+	[SerializeField]
+	private PHASE _phase;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public PHASE getPhase(){
+		return _phase;
+	}
+
+	public void setPhase( PHASE phase ){
+		_phase = phase;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
