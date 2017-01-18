adcc8e225f253d6119041116a56c6c741edf21e9
Yuu <remillyacross@gmail.com>
2017-01-18 18:31:46 +0900

    no message

--- boardgameFlow/Assets/Scripts/ClientPlayerManager.cs
+++ boardgameFlow/Assets/Scripts/ClientPlayerManager.cs
@@ -12,33 +12,55 @@ public class ClientPlayerManager : MonoBehaviour {
 		public List<GameObject> hand_obj_list;
 		public List<Vector3>    select_position;
 	}
-
+	[SerializeField]
+	private GameObject _hand_Area; 
+	[SerializeField]
 	private PLAYER_CARD_DATA _player_card = new PLAYER_CARD_DATA( );
+	[SerializeField]
+	private CardManager _card_manager;
 	// Use this for initialization
 	void Start () {
-	
+		_player_card.hand_list = new List<CARD_DATA>( );
+		_player_card.hand_obj_list = new List<GameObject>( );
+		_player_card.select_position = new List<Vector3>( );
+		if ( _hand_Area == null ) {
+			_hand_Area = GameObject.Find( "HandArea" );
+		}
+		if ( _card_obj == null ) {
+			_card_obj = (GameObject)Resources.Load("Prefabs/Card");
+		}
+		if ( _card_manager == null ) {
+			_card_manager = GameObject.Find( "CardManager" ).GetComponent<CardManager>( );
+		}
 	}
 	
 	// Update is called once per frame
+	#if UNITY_EDITOR
 	void Update () {
-	
+		if ( Input.GetKeyDown( KeyCode.X ) ) {
+			deckCardList( 1 );
+			deckCardList( 1 );
+			deckCardList( 1 );
+			deckCardList( 1 );
+			deckCardList( 1 );
+		}
 	}
+	#endif
 	//カード生成を行う
-	public void deckCardList( int get_card_id ) {
+	private void deckCardList( int get_card_id ) {
 		CARD_DATA card;
-		CardManager card_manager = GameObject.Find ( "CardManager" ).GetComponent< CardManager >( );
 
 		//IDのカードデータを取得
-		card = card_manager.getCardData( get_card_id );
+		card = _card_manager.getCardData( get_card_id );
 
 		//最新のカードを生成
 		AllHandCreate( card );
 	}
 	//現在のの手札の生成を行う
-	public void AllHandCreate( CARD_DATA setCard ) {
-
+	public void AllHandCreate( CARD_DATA card_data ) {
+		Debug.Log(card_data.name);
 		//カードを手札に追加
-		_player_card.hand_list.Add( setCard );
+		_player_card.hand_list.Add( card_data );
 
 		//現在のカードを削除
 		for ( int i = _player_card.hand_obj_list.Count - 1; i >= 0; i-- ) {
@@ -52,9 +74,9 @@ public class ClientPlayerManager : MonoBehaviour {
 			//プレハブを生成してリストのオブジェクトに入れる
 			_player_card.hand_obj_list.Add( ( GameObject )Instantiate( _card_obj ) );
 			//カードデータ設定
-			//_player_card.hand_obj_list[ i ].GetComponent< Card >( ).setCardData( _player_card.hand_list[ i ] );
-			//スタート地点を取得
-		//	float start_Card_Point = ( handArea_Width_Size / 2 ) - _player_card.hand_obj_list[ i ].transform.localScale.x;
+			_player_card.hand_obj_list[ i ].GetComponent< Card >( ).setCardData( _player_card.hand_list[ i ] );
+			float handArea_Width_Size = _hand_Area.GetComponent< Transform >( ).localScale.x;
+			float start_Card_Point = ( handArea_Width_Size / 2 ) - _player_card.hand_obj_list[ i ].transform.localScale.x;
 			//手札が6枚以下なら
 			//カード間に現在の生成中の手札の順番を掛ける
 		//	card_X = -start_Card_Point + ( handArea_Width_Size / _player_card.hand_list.Count ) * i;
